ee97bfba664e99382b80e92d15b87532c81bf8a9
gjfranics9 <67798013+gjfranics9@users.noreply.github.com>
2020-09-29 15:01:23 +0100

    Update Dokkan_calculator.py

--- Dokkan_calculator.py
+++ Dokkan_calculator.py
@@ -1,17 +1,21 @@
 class attacking_char:
-
+#this is object oriented for almost no reason
     def __init__(self,base_atk,leader_stat,friend_stat):
+        #sets base for all stats currently
         self.base_atk = 0
         self.leader_stat = 0
         self.friend_stat = 0
 
     def stats(self):
+        #an output to check current stats and output, used to verify everything
         return{'base attack': self.base_atk, "leader skill": self.leader_stat, "friend's leader skill": self.friend_stat}
     
     def base_attack(self):
         #base attack
         while True:
+            #validation for integer values
             try:
+                #base atk retrieve
                 self.base_atk = int(input("Please input the attack stat of the unit (include hidden potential buffs)"))
                 return self.base_atk
                 break
@@ -22,6 +26,7 @@ class attacking_char:
         #primary leader skill
         while True:
             try:
+                #primary leader retrieve
                 self.leader_stat = int(input("Please input atk bonus percentage from leader (Nuke leaders not yet implemented)"))
                 if self.leader_stat < 0 or self.leader_stat > 177:
                     continue
@@ -34,6 +39,7 @@ class attacking_char:
         #secondary leader skill
         while True:
             try:
+                #friend leader retrieve
                 self.friend_stat = int(input("Please input atk bonus percentage from friend leader (Nuke leaders not yet implemented)"))
                 if self.friend_stat < 0 or self.friend_stat > 177:
                     continue
@@ -43,17 +49,17 @@ class attacking_char:
                 continue
 
     def leaderSkills_multiplier(self,leader_stat,friend_stat):
+        #leader multiplier is +total %. This finds the multiplier
         leader_stat += 100
         leader_stat += friend_stat
         leader_multiplier = leader_stat/100
         return leader_multiplier
 
     def leaderBuffed_atk(self, leader_multiplier, base_atk):
+        #temporary value that calculates the first step of damage calculation
         return leader_multiplier * base_atk
 
 
 unit = attacking_char(0,0,0)
-base_attack = unit.base_attack()
-leader_multiplier = (unit.leaderSkills_multiplier(unit.leader_skill(),unit.friend_skill()))
-leaderskillin = base_attack * leader_multiplier
-print(attackxleaders)
+leaderskillin = unit.base_attack() * (unit.leaderSkills_multiplier(unit.leader_skill(),unit.friend_skill()))
+print(leaderskillin)
